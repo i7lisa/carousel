a63a51d510020fe08ace0f158d6d2572c05afb48
i7lisa <lisaoubykin@gmail.com>
2025-11-11 11:57:33 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+let nextButton = document.getElementById('next');
+let preButton = document.getElementById('pre');
+let backButton = document.getElementById('back');
+let seeMoreButtons = document.querySelectorAll('.seeMore');
+let carousel = document.querySelector('.carousel');
+let listHTML = document.querySelector('.carousel .list');
+
+nextButton.oneclick = function(){
+    showSlider('next');
+}
+preButton.oneclick = function(){
+    showSlider(pre);
+}
+const showSlider = (type) => {
+    let items = document.querySelectorAll('.carousel .list .item');
+    if(type === 'next'){
+        listHTML.appendChild(items[0]);
+    }
+}
