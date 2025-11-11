14a58c8fa141e2cb3d95e9943f046ab65bdbfdf5
i7lisa <lisaoubykin@gmail.com>
2025-11-11 13:54:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 let nextButton = document.getElementById('next');
-let preButton = document.getElementById('pre');
+let prevButton = document.getElementById('prev');
 let backButton = document.getElementById('back');
 let seeMoreButtons = document.querySelectorAll('.seeMore');
 let carousel = document.querySelector('.carousel');
 let listHTML = document.querySelector('.carousel .list');
 
-nextButton.oneclick = function(){
+nextButton.onclick = function(){
     showSlider('next');
 }
-preButton.oneclick = function(){
-    showSlider(pre);
+prevButton.onclick = function(){
+    showSlider('prev');
 }
+let unAcceptClick;
 const showSlider = (type) => {
+    nextButton.style.pointerEvents = 'none';
+    prevButton.style.pointerEvents = 'none';
+
+    carousel.classList.remove('prev', 'next');
     let items = document.querySelectorAll('.carousel .list .item');
     if(type === 'next'){
         listHTML.appendChild(items[0]);
         carousel.classList.add('next')
+    }else{
+        let positionLast = items.length -1;
+        listHTML.prepend(items[positionLast]);
+        carousel.classList.add('prev');
     }
+
+    clearTimeout(unAcceptClick);
+    unAcceptClick = setTimeout(() => {
+    nextButton.style.pointerEvents = 'auto';
+    prevButton.style.pointerEvents = 'auto';
+    }, 2000);
 }
